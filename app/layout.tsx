3d5cfb60037b7fd319ccdd391859d33655c9c597
Philipp Runge <p.runge@progani.com>
2025-07-14 00:51:48 +0200

    add logo and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Pokémon Collection Tracker",
+  title: "TCG Collector",
   description:
     "Track your Pokémon card collection with detailed information about sets, languages, variants, and conditions.",
-}
+  icons: [{ rel: "icon", url: "/favicon.png" }],
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,13 +1,23 @@
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Navigation() {
   return (
     <header className="bg-white shadow-sm border-b">
       <div className="container mx-auto px-4 py-4">
         <nav className="flex justify-between items-center">
-          <Link href="/" className="text-2xl font-bold text-gray-900">
-            Pokémon Tracker
+          <Link
+            href="/"
+            className="flex items-center text-2xl font-bold text-gray-900 gap-2"
+          >
+            <Image
+              src="/logo.png"
+              alt="TCG Collector Logo"
+              width={75}
+              height={60}
+            />
+            TCG Collector
           </Link>
           <div className="flex items-center gap-4">
             <Link href="/">
