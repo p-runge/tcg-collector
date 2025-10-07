4d3c2b94648f776b07bf7f8c9ae20f5aa10b1756
Philipp Runge <p.runge@progani.com>
2025-10-07 23:57:41 +0200

    add seeding endpoint triggering the api fetching and data storing script

---
+++ app/api/seed/route.ts
@@ -0,0 +1,15 @@
+import { fetchAndStoreSets } from "@/lib/db/scripts";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  // Example seed logic
+  const seedData = {
+    message: "Database seeded successfully!",
+    timestamp: new Date().toISOString(),
+  };
+
+  fetchAndStoreSets();
+
+  // Return a JSON response
+  return NextResponse.json(seedData);
+}
--- lib/db/scripts.ts
+++ lib/db/scripts.ts
@@ -3,11 +3,14 @@ import { db } from ".";
 import { setsTable } from "./index";
 import pokemonAPI from "../pokemon-api";
 
-async function fetchAndStoreSets() {
+export async function fetchAndStoreSets() {
+  console.log("Fetching and storing sets...");
   try {
     const sets = await pokemonAPI.fetchPokemonSets();
+    console.log(`Fetched ${sets.length} sets from the API.`);
 
     for (const set of sets) {
+      console.log(`Storing set: ${set.name} (${set.id})`, set);
       await db.insert(setsTable).values({
         id: set.id,
         name: set.name,
@@ -18,6 +21,7 @@ async function fetchAndStoreSets() {
         totalWithSecretRares: set.totalWithSecretRares,
         series_id: set.series,
       });
+      console.log(`Stored set: ${set.name} (${set.id})`);
     }
 
     console.log("Sets have been successfully fetched and stored.");
