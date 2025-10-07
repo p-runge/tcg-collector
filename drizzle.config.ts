8fb70677b4937eadb68516a0969e462abb48fb5e
Philipp Runge <p.runge@progani.com>
2025-10-07 23:52:09 +0200

    fix drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./lib/db.ts",
+  schema: "./lib/db/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL,
--- lib/db/index.ts
+++ lib/db/index.ts
@@ -18,7 +18,7 @@ export const usersTable = pgTable("users", {
   discord_id: varchar("discord_id", { length: 32 }).notNull(),
   username: varchar("username", { length: 64 }).notNull(),
   avatar_url: text("avatar_url"),
-  favorite_language_id: integer("favorite_language_id"),
+  favorite_language_id: uuid("favorite_language_id"),
   created_at: timestamp("created_at", { mode: "string" })
     .notNull()
     .defaultNow(),
@@ -57,7 +57,7 @@ export const setsTable = pgTable("sets", {
 // Card table
 export const cardsTable = pgTable("cards", {
   id: uuid("id").primaryKey(),
-  set_id: integer("set_id")
+  set_id: uuid("set_id")
     .notNull()
     .references(() => setsTable.id),
   number: varchar("number", { length: 32 }).notNull(),
@@ -94,11 +94,11 @@ export const cardsTable = pgTable("cards", {
 // UserCard table
 export const userCardsTable = pgTable("user_cards", {
   id: uuid("id").primaryKey(),
-  user_id: integer("user_id").notNull(),
-  card_id: integer("card_id").notNull(),
-  language_id: integer("language_id").notNull(),
-  variant_id: integer("variant_id").notNull(),
-  condition_id: integer("condition_id").notNull(),
+  user_id: uuid("user_id").notNull(),
+  card_id: uuid("card_id").notNull(),
+  language_id: uuid("language_id").notNull(),
+  variant_id: uuid("variant_id").notNull(),
+  condition_id: uuid("condition_id").notNull(),
   quantity: integer("quantity").notNull(),
   notes: text("notes"),
   created_at: timestamp("created_at", { mode: "string" })
@@ -112,7 +112,7 @@ export const userCardsTable = pgTable("user_cards", {
 // // UserCollectingSet table
 // export const userCollectingSetsTable = pgTable("user_collecting_sets", {
 //   id: uuid("id").primaryKey(),
-//   user_id: integer("user_id").notNull(),
-//   set_id: integer("set_id").notNull(),
+//   user_id: uuid("user_id").notNull(),
+//   set_id: uuid("set_id").notNull(),
 //   created_at: timestamp("created_at", { mode: "string" }).notNull().defaultNow(),
 // });
