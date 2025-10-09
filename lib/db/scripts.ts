7a1f10cc2dcc2934f14836049cbf88aa71b4c7d2
Philipp Runge <p.runge@progani.com>
2025-10-09 23:47:53 +0200

    also fetch cards when seeding

--- lib/db/scripts.ts
+++ lib/db/scripts.ts
@@ -1,9 +1,9 @@
 // fetch sets from external API and insert into the database
-import { db } from ".";
+import { cardsTable, db } from ".";
 import { setsTable } from "./index";
 import pokemonAPI from "../pokemon-api";
 
-export async function fetchAndStoreSets() {
+export async function fetchAndStoreSets(withCards = true) {
   console.log("Fetching and storing sets...");
   try {
     const sets = await pokemonAPI.fetchPokemonSets();
@@ -37,6 +37,8 @@ export async function fetchAndStoreSets() {
           },
         });
       console.log(`Stored set: ${set.name} (${set.id})`);
+
+      if (withCards) await fetchAndStoreCards(set.id);
     }
 
     console.log("Sets have been successfully fetched and stored.");
@@ -45,4 +47,42 @@ export async function fetchAndStoreSets() {
   }
 }
 
-fetchAndStoreSets();
+export async function fetchAndStoreCards(setId: string) {
+  console.log("Fetching and storing cards...");
+  try {
+    const cards = await pokemonAPI.fetchPokemonCards(setId);
+    console.log(`Fetched ${cards.length} cards from the API.`);
+    console.log("cards", cards);
+
+    for (const card of cards) {
+      console.log(`Storing card: ${card.name} (${card.id})`, card);
+      await db
+        .insert(cardsTable)
+        .values({
+          id: card.id,
+          name: card.name,
+          number: card.number,
+          rarity: card.rarity,
+          imageSmall: card.images.small,
+          imageLarge: card.images.large,
+          setId: card.set.id,
+        })
+        .onConflictDoUpdate({
+          target: cardsTable.id,
+          set: {
+            name: card.name,
+            number: card.number,
+            rarity: card.rarity,
+            imageSmall: card.images.small,
+            imageLarge: card.images.large,
+            setId: card.set.id,
+          },
+        });
+      console.log(`Stored card: ${card.name} (${card.id})`);
+    }
+
+    console.log("Cards have been successfully fetched and stored.");
+  } catch (error) {
+    console.error("Error fetching or storing cards:", error);
+  }
+}
