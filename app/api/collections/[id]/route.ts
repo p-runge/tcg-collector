d0af0fa782e36f46908f793a00ebfa1324a2d8a8
Philipp Runge <p.runge@progani.com>
2025-10-17 01:18:26 +0200

    add put endpoint for collection

--- app/api/collections/[id]/route.ts
+++ app/api/collections/[id]/route.ts
@@ -24,3 +24,34 @@ export async function GET(
     headers: { "Content-Type": "application/json" },
   });
 }
+
+type UpdateCollectionRequest = {
+  name: string;
+};
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  const { name } = (await req.json()) as UpdateCollectionRequest;
+
+  if (!name || typeof name !== "string") {
+    return new Response("Invalid collection name", { status: 400 });
+  }
+
+  const result = await db
+    .update(collectionsTable)
+    .set({ name })
+    .where(eq(collectionsTable.id, id))
+    .returning()
+    .then((res) => res[0]);
+
+  if (!result) {
+    return new Response("Collection not found", { status: 404 });
+  }
+
+  return new Response(JSON.stringify(result), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+}
