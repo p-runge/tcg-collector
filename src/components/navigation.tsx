3d58d49879ad62f8fe96c1a832055d251538b50f
Philipp Runge <p.runge@progani.com>
2025-10-26 01:33:31 +0200

    rebrand the navigation

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -14,23 +14,25 @@ export async function Navigation() {
     <header className="shadow-sm border-b">
       <div className="container mx-auto px-4 py-4">
         <nav className="flex justify-between items-center">
-          <Link
-            href="/"
-            className="flex items-center text-2xl font-bold gap-2"
-          >
-            <Image
-              src="/logo.png"
-              alt={
-                intl.formatMessage({ id: "navigation.logoAlt", defaultMessage: "{logoName} Logo" }, { logoName: "TCG Collector" })
-              }
-              width={75}
-              height={60}
-            />
-            TCG Collector
-          </Link>
-
-          <LanguageDropdown />
-          <DarkModeToggle />
+          <div className="flex items-center gap-3">
+            <Link
+              href="/"
+              className="flex items-center text-2xl font-bold gap-2"
+            >
+              <Image
+                src="/bulkratte_logo.png"
+                alt={
+                  intl.formatMessage({ id: "navigation.logoAlt", defaultMessage: "{logoName} Logo" }, { logoName: "TCG Collector" })
+                }
+                width={80}
+                height={80}
+                // the file has padding itself, so we counter the header padding
+                className="-my-4"
+              />
+            </Link>
+            <LanguageDropdown />
+            <DarkModeToggle />
+          </div>
 
           <div className="flex items-center gap-4">
             <Link href="/">
